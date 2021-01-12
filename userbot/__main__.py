96f4990ee4b4b64e96999e239792a52fafbbca9d
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-11 22:30:52 -0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay your userbot is officially working.")
+print("ஆம்! உங்கள் பயனர் போட் அதிகாரப்பூர்வமாக செயல்படுகிறது 🥳.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
