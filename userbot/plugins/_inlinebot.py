f75129e7915961c42bbeb625c050d9fcf316fbd6
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-12 21:20:44 -0800

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use another's\n ib @IMVETRI for learning how to get userbot!"
+            reply_pop_up_alert = "Get your own userbot, don't use another's\n Ask Here @TamilSupport for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,7 +55,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use another's\n ib @IMVETRI for learning how to get userbot!"
+            reply_pop_up_alert = "Get your own userbot, don't use another's\n Ask Here @TamilSupport for learning how to get userbot!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
