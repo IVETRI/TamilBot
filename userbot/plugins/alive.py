968ccdca961d089b2c9833a86c5cf769c5aaf83e
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-15 07:26:53 -0800

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,15 +13,15 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`வணக்கம்! (●'◡'●) \nஉங்கள் Bot இயங்குகிறது.\n\n`"
-                     f"`எனது உரிமையாளர்👨🏻‍💻`: {DEFAULTUSER}\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\n"
-                     "`🤖 Provided by: @TamilUserBot.\n\n`"
-                     "`தரவுத்தள நிலை📶: தரவுத்தளங்கள் சிறப்பாக செயல்படுகின்றன🥳!\n\nஎன்றும் உன்னுடன்🌺,\n`"
-                     "[Tamil UserBot](https://t.me/TamilSupport)❤️"
+    await alive.edit("`வணக்கம்! (●'◡'●) \n**உங்கள் Bot இயங்குகிறது.**\n\n`"
+                     f"`❄)எனது உரிமையாளர்👨🏻‍💻`: {DEFAULTUSER}\n"
+                     "`❄)Telethon version: 6.9.0\nPython: 3.7.3\n"
+                     "`❄)🤖 Provided by: @TamilUserBot.\n`"
+                     "❄)தரவுத்தள நிலை📶: தரவுத்தளங்கள் சிறப்பாக செயல்படுகின்றன🥳!\n\nஎன்றும் உன்னுடன்🌺,\n`"
+                     "[Tamil UserBot](https://t.me/TamilSupport)❤️\n\n`"
                      "[Deploy this userbot Now](https://github.com/ivetri/tamilbot)")
 
 @borg.on(admin_cmd(pattern=r"sudoalive", allow_sudo=True))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`thanks For using me🤖")
+    await alive.edit("`என்னைப் பயன்படுத்தியதற்கு நன்றி🤖")
