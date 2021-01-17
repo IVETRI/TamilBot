3a934850bb04149f24417df822dc0d272d5d6c82
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-17 02:33:48 -0800

    Create ss.py

---
+++ userbot/plugins/ss.py
@@ -0,0 +1,20 @@
+from telethon import functions
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="ss"))
+async def _(event):
+    if event.fwd_from:
+        return
+    NO_OF_SCSS = int(event.text.split(" ", maxsplit=1)[1])
+    event.message.id
+    if event.reply_to_msg_id:
+        event.reply_to_msg_id
+
+    for I in range(NO_OF_SCSS):
+        await event.client(
+            functions.messages.SendScreenshotNotificationRequest(
+                peer=event.chat_id, reply_to_msg_id=42
+            )
+        )
+    await event.delete()
