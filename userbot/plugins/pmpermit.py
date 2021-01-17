e11862e99ab86c8af89b4e68ae48829b7f4b06f8
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-17 10:21:34 -0800

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -7,7 +7,6 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from userbot import ALIVE_NAME
-from userbot.Configs import Config
 from userbot.utils import admin_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
