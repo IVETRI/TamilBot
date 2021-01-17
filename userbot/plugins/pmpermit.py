01aeac671d4d356cf442b1da5833e5b890a5765a
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-17 10:09:28 -0800

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -6,7 +6,7 @@ from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
-from userbot import ALIVE_NAME, CUSTOM_PMPERMIT
+from userbot import ALIVE_NAME
 from userbot.Configs import Config
 from userbot.utils import admin_cmd
 
