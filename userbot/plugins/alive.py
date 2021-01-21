b40d3d9a03f0f34fc0e879d1a6224bb3dce5f997
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-01-22 00:53:25 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,25 +1,34 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-import asyncio
-from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
-from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
+"""Check if tamilbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+# CREDITS: @WhySooSerious, @Sur_vivor
+# modified by @saravanakrish
+from userbot.util import admin_cmd
+
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.modules import currentversion
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "tamiluserbot"
+PM_IMG = "https://telegra.ph/file/cb72a96f40d9026e3341d.jpg"
+pm_caption = "➥ **TAMILBOT IS:** `ONLINE`\n\n"
+pm_caption += "➥ **SYSTEMS STATS**\n"
+pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
+pm_caption += "➥ **Python:** `3.7.4` \n"
+pm_caption += "➥ **Database Status:**  `Functional`\n"
+pm_caption += "➥ **Current Branch** : `master`\n"
+pm_caption += f"➥ **Version** : `6.5`\n"
+pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
+pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/ivetri/tamilbot/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [ivetri@Github](GitHub.com/ivetri)\n"
+pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
+pm_caption += "[🇮🇳 Deploy TamilUserbot 🇮🇳](https://t.me/tamiluserbot)"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-
-#@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
-async def amireallyalive(alive):
+async def tamilbot(alive):
+    await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`வணக்கம்! (●'◡'●) \n**உங்கள் Bot இயங்குகிறது.**\n\n`"
-                     f"`❄)எனது உரிமையாளர்👨🏻‍💻`: {DEFAULTUSER}\n"
-                     "`❄)Telethon version: 6.9.0\nPython: 3.7.3\n"
-                     "`❄)🤖 Provided by: @TamilUserBot.\n`"
-                     "❄)தரவுத்தள நிலை📶: தரவுத்தளங்கள் சிறப்பாக செயல்படுகின்றன🥳!\n\nஎன்றும் உன்னுடன்🌺,\n`"
-                     "[Tamil UserBot](https://t.me/TamilSupport)❤️\n\n`"
-                     "[Deploy this userbot Now](https://github.com/ivetri/tamilbot)")
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
+    await alive.delete()
 
 @borg.on(admin_cmd(pattern=r"sudoalive", allow_sudo=True))
 async def amireallyalive(alive):
