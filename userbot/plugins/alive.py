63fa3c9fb1ff4fd8b0d414609a013f7aa9d0f610
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-01-22 22:31:36 -0800

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,7 +1,8 @@
-"""Check if tamilbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+"""Check if tamilBot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 # CREDITS: @WhySooSerious, @Sur_vivor
 
 # modified by @saravanakrish
+# Re-written by @iMvEtRi
 from userbot.utils import admin_cmd
 from userbot.uniborgConfig import Config
 from userbot import ALIVE_NAME, CMD_HELP
@@ -10,16 +11,16 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "tamiluserbot"
 
 PM_IMG = Config.ALIVE_IMAGE
 pm_caption = "➥ **TAMILBOT IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.7.4` \n"
-pm_caption += "➥ **Database Status:**  `Functional`\n"
+pm_caption += "➥ **SYSTEMS STATS👨🏻‍💻:**\n"
+pm_caption += "➥ **Telethon Version :** `1.15.0` \n"
+pm_caption += "➥ **Python :** `3.7.4` \n"
+pm_caption += "➥ **Database Status :**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `6.5`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/ivetri/tamilbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [ivetri@Github](GitHub.com/ivetri)\n"
+pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`💥\n\n"
+pm_caption += "➥ **License** : [MIT License](github.com/ivetri/tamilbot/blob/master/LICENSE)✔\n"
+pm_caption += "➥ **Copyrights** : © By [IVETRI](GitHub.com/ivetri)\n"
 pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
 pm_caption += "[🇮🇳 TamilUserbot 🇮🇳](https://t.me/tamiluserbot)"
 
