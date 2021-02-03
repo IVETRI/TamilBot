ddc961cf2f6012fd2ce9418e2191066f7aab569f
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-02-03 22:10:32 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -34,9 +34,9 @@ async def inline_handler(event):
     query = event.text
     if event.query.user_id == bot.uid and query.startswith("TamilBot"):
         rev_text = query[::-1]
-        buttons = paginate_help(0,"helpme")
+        buttons = paginate_help(0, CMD_LIST, "helpme")
         result = builder.article(
-            "© Userbot Help",
+            "© Tamilbot Help",
             text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
             buttons=buttons,
             link_preview=False,
