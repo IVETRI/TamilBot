04b3060f6bc3f65ce329126937febaff8c5486b7
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-02-12 21:36:14 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,5 +1,12 @@
+# rewritten by @saravanakrish
+
 from telethon import events
 from datetime import datetime
+
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd, edit_or_reply
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TamilBot🇮🇳"
 
 
 @command(pattern="^.ping")
@@ -10,4 +17,7 @@ async def _(event):
     await event.edit("Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit("🏓 Pong!\n{}".format(ms))
+    await event.edit(
+        f"__**🚴🏻‍♂️🚴🏻‍♂️ Pong!__**\n➥__**Ping Speed**__ {ms}\n➥ __**Bot**__ __**of**__ [{DEFAULTUSER}](tg://user?id={SURID})"
+    )
+
