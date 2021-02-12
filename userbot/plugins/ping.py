34ad9d1ebce50a6285e9aa18c071d4217717006e
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-02-12 22:45:28 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -13,11 +13,13 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TamilBot🇮🇳"
 async def _(event):
     if event.fwd_from:
         return
+
+    IMSID = bot.uid
     start = datetime.now()
     event = await edit_or_reply(event, "__**🚴🏻‍♂️ Pong!__**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(
-        f"__**🚴🏻‍♂️ Pong!__**\n➥__**Ping Speed**__ {ms}\n➥ __**Bot**__ __**of**__ [{DEFAULTUSER}](tg://user?id={SURID})"
+        f"__**🚴🏻‍♂️ Pong!__**\n➥__**Ping Speed**__ {ms}\n➥ __**Bot**__ __**of**__ [{DEFAULTUSER}](tg://user?id={IMSID})"
     )
 
