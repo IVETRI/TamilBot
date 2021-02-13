4588a8c3163e691cd24187601371068c5c5f612f
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-02-13 07:57:25 -0800

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -1,17 +1,84 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="tagall"))
+@borg.on(admin_cmd(pattern="tagall$"))
 async def _(event):
     if event.fwd_from:
         return
+    reply_to_id = event.message
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
     mentions = "@tagall"
     chat = await event.get_input_chat()
-    async for x in borg.iter_participants(chat, 100):
+    async for x in event.client.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
-    await event.reply(mentions)
+    await reply_to_id.reply(mentions)
     await event.delete()
+
+
+@borg.on(admin_cmd(pattern="all( (.*)|$)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    input_str = event.pattern_match.group(1)
+    mentions = input_str or "@tagall"
+    chat = await event.get_input_chat()
+    async for x in event.client.iter_participants(chat, 100):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    await event.client.send_message(event.chat_id, mentions, reply_to=reply_to_id)
+    await event.delete()
+
+
+@borg.on(admin_cmd(pattern="report$"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "@admin: **Spam Spotted**"
+    chat = await event.get_input_chat()
+    reply_to_id = await reply_id(event)
+    async for x in event.client.iter_participants(
+        chat, filter=ChannelParticipantsAdmins
+    ):
+        if not x.bot:
+            mentions += f"[\u2063](tg://user?id={x.id})"
+    await event.client.send_message(event.chat_id, mentions, reply_to=reply_to_id)
+    await event.delete()
+
+
+@borg.on(admin_cmd(pattern="men (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    reply_to_id = await reply_id(event)
+    if event.reply_to_msg_id:
+        reply_msg = await event.get_reply_message()
+        u = reply_msg.sender_id
+        str = input_str
+    else:
+        user, str = input_str.split(" ", 1)
+        try:
+            u = int(user)
+        except ValueError:
+            try:
+                u = await event.client.get_entity(user)
+            except ValueError:
+                await event.delete()
+                return
+            u = int(u.id)
+        except:
+            await event.delete()
+            return
+    await event.delete()
+    await event.client.send_message(
+        event.chat_id,
+        f"<a href='tg://user?id={u}'>{str}</a>",
+        parse_mode="HTML",
+        reply_to=reply_to_id,
+    )
+
