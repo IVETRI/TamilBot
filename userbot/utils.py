9b5ffe340b9a39d6c6dee6c9546485792eee2e0c
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-18 04:45:58 -0700

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -86,7 +86,7 @@ def load_module(shortname):
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(mod)
-        print("Successfully (re)imported "+shortname)
+        print("Successfully imported "+shortname)
     else:
         import userbot.utils
         import sys
@@ -110,7 +110,7 @@ def load_module(shortname):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["userbot.plugins."+shortname] = mod
-        print("Successfully (re)imported "+shortname)
+        print("Successfully imported "+shortname)
 
 def remove_plugin(shortname):
     try:
