e0279324017583484982f8cfc258b8eadb3b9273
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-03-18 06:28:11 -0700

    Update fedstuff.py

--- userbot/plugins/fedstuff.py
+++ userbot/plugins/fedstuff.py
@@ -11,14 +11,25 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import os
 import asyncio
 
-from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.errors import ChatAdminRequiredError
+from telethon.errors.rpcerrorlist import MessageTooLongError, YouBlockedUserError
+from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot.utils import admin_cmd
+from userbot import ALIVE_NAME
 
 bot = "@MissRose_bot"
 
+naam = str(ALIVE_NAME)
+
+BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+
+G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", None)
+if G_BAN_LOGGER_GROUP:
+    G_BAN_LOGGER_GROUP = int(G_BAN_LOGGER_GROUP)
 
 @borg.on(admin_cmd(pattern="fstat ?(.*)"))
 async def _(event):
@@ -75,3 +86,101 @@ async def _(event):
             await ok.edit(audio.text + "\n\nFedInfo Excracted by TamilBot")
         except YouBlockedUserError:
             await ok.edit("**Error**\n `Unblock` @MissRose_Bot `and try again!")
+
+@borg.on(admin_cmd("roseinfo ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    sysarg = event.pattern_match.group(1)
+    if sysarg == "":
+        async with borg.conversation(bots) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/info")
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("**Error:** `unblock` @MissRose_bot `and retry!")
+    elif "@" in sysarg:
+        async with borg.conversation(bots) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/info " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+    elif "" in sysarg:
+        async with borg.conversation(bots) as conv:
+            try:
+                await conv.send_message("/start")
+                await conv.get_response()
+                await conv.send_message("/info " + sysarg)
+                audio = await conv.get_response()
+                await borg.send_message(event.chat_id, audio.text)
+                await event.delete()
+            except YouBlockedUserError:
+                await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+
+@borg.on(admin_cmd("myfeds ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    async with event.client.conversation(bots) as conv:
+        try:
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.send_message("/myfeds")
+            myfed = await conv.get_response()
+            if "file" in myfed.text:
+                await fedstat.click(0)
+                reply = await conv.get_response()
+                await event.client.forward_messages(event.chat_id, reply)
+            else:
+                await event.client.forward_messages(event.chat_id, myfed)
+                await event.delete()
+        except YouBlockedUserError:
+            await event.edit("**Error:** `unblock` @MissRose_Bot `and try again!")
+
+
+@borg.on(admin_cmd(pattern="bgban ?(.*)"))
+async def _(event):
+    if G_BAN_LOGGER_GROUP is None:
+        await event.edit("ENV VAR is not set. This module will not work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        if r.forward:
+            r_from_id = r.forward.from_id or r.from_id
+        else:
+            r_from_id = r.from_id
+        await borg.send_message(
+            G_BAN_LOGGER_GROUP,
+            "/gban [user](tg://user?id={}) {}".format(r_from_id, reason),
+        )
+    await event.delete()
+
+
+@borg.on(admin_cmd(pattern="bungban ?(.*)"))
+async def _(event):
+    if G_BAN_LOGGER_GROUP is None:
+        await event.edit("ENV VAR is not set. This module will not work.")
+        return
+    if event.fwd_from:
+        return
+    reason = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        r = await event.get_reply_message()
+        r_from_id = r.from_id
+        await borg.send_message(
+            G_BAN_LOGGER_GROUP,
+            "/ungban [user](tg://user?id={}) {}".format(r_from_id, reason),
+        )
+    await event.delete()
